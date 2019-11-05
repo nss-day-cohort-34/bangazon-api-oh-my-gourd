69d4db61c3b290c7b260a80e8e8c0641b46eaa83
Will Wilkinson <wfw40869@gmail.com>
2019-11-05 16:08:38 -0600

    CRUD is working. Added _include=payments query parameter logic for get all customers

---
+++ BangazonAPI/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int ProductTypeId { get; set; }
+        public int SellerId { get; set; }
+        public double Price { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+    }
+}
